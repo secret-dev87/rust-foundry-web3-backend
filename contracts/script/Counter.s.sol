c2140e45d89f8dfc7628830b3c2d83e912818937
secret-dev87 <dev.bruce.87@gmail.com>
2023-12-08 03:20:27 -0500

    build and deploy the smart contract

--- contracts/script/Counter.s.sol
+++ contracts/script/Counter.s.sol
@@ -2,11 +2,20 @@
 pragma solidity ^0.8.13;
 
 import {Script, console2} from "forge-std/Script.sol";
+import {Counter} from "../src/Counter.sol";
 
 contract CounterScript is Script {
     function setUp() public {}
 
+    // Deployed to 0x2A872b3411b7d9B006Ca16600B51D742da7e6089
     function run() public {
-        vm.broadcast();
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        vm.startBroadcast(deployerPrivateKey);
+
+        Counter counter = new Counter();
+        console2.log("Counter address: ", address(counter));
+        counter.setNumber(69420);
+        console2.log("Counter number: ", counter.number());
+        vm.stopBroadcast();
     }
 }
